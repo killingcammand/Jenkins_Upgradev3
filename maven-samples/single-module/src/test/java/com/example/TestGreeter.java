2e9fbbeb6d56e3f25dac3af7cc65da3150d93edd
killingcammand <154828355+killingcammand@users.noreply.github.com>
2026-01-25 00:20:30 +0530

    Add test for greeting message inclusion

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,5 +41,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+   @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  
 
 }
