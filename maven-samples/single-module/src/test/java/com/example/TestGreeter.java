0d95eeac066cabd2dae4e24dd26ac17852943303
killingcammand <154828355+killingcammand@users.noreply.github.com>
2026-01-24 23:56:53 +0530

    Add intentionally_failed test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,8 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+     @Test
+  public void intentionally_failed() {
+    assertTrue(false);
+  }
 }
